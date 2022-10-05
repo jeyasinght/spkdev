7b2409a0023edd5b16282dc924e3746ddf0dbde9
jeyasinght <jeyasingh.t@gmail.com>
2022-10-05 11:00:33 +0800

    version updated

--- spkapp/apps.py
+++ spkapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SpkprodappConfig(AppConfig):
+class SpkappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'spkapp'
